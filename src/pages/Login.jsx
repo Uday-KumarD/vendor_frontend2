99396de827cbe4c1d5a4ce4e011b73321f0ca59a
Uday-KumarD <doddaudaykumar@gmail.com>
2025-06-27 00:18:46 +0530

    corrected login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 function Login() {
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:5000/api/auth/google';
+    window.location.href = 'https://vendor-backend3.onrender.com/api/auth/google';
   };
 
   return (
